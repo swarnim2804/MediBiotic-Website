2a74e6558dbf3999ce0f5a21fefef943a0617c47
Swarnim Sahay <s.sahay2354@gmail.com>
2025-06-02 17:48:05 +0530

    Update routing to use HashRouter in App.js for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from "./components/LandingPage";
 import Medibot from "./components/Medibot";
 import NearbyServices from "./components/NearbyServices";
